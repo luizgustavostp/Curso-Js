78834decd12a5f72a725bfd05b121c4c137ff6f7
Luiz Gustavo Dos Santos Pereira <luizgustavosantospreira@gmail.com>
2025-03-16 09:09:31 -0300

    Add files via upload

--- calendario/script.js
+++ calendario/script.js
@@ -2,6 +2,11 @@ let data = new Date
 let mes = data.getMonth()
 let ano = data.getFullYear()
 let dia = data.getUTCDate()
+
+let dataatual = data
+let anoatual = ano
+let mesatual = mes
+let diaatual = dia
 const calendar = document.getElementById("calendar")
 const monthtxt = document.getElementById('monthtxt')
 let meses = ["janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"]
@@ -16,11 +21,15 @@ function avancar(data) {
         let item = document.createElement('div')
         item.innerHTML = i
         item.setAttribute('class',"calendar-day")
+        if (mespg == mesatual && ano == anoatual && i == dia) {
+        item.style.backgroundColor = 'rgba(60, 208, 208, 0.732)'
+        }
         calendar.appendChild(item)
+        
     }
     mespg++
     if (mespg > 11 ) {
-        mespg = 0
+        mespg = 1
     
     }
     
@@ -32,18 +41,25 @@ function PegardiasdoMesAnterior(data) {
     return new Date(data.getFullYear(),mespg, 0).getDate();
 }
 function voltar(data) {
-    calendar.innerHTML = ""
-    for (let ii = 1;ii <= PegardiasdoMesAnterior(data);ii++) {
-        let item = document.createElement('div')
-        item.innerHTML = ii
-        item.setAttribute('class',"calendar-day")
-        calendar.appendChild(item)
-    }
     mespg--
     if (mespg < 1) {
         mespg = 11
     }
+    calendar.innerHTML = ""
+    for (let ii = 1;ii <= PegardiasdoMesAnterior(data);ii++) {
+        let item = document.createElement('div')
+        item.innerHTML = ii
+        let meso = mespg - 1
+        if (meso == mesatual && ano == anoatual && ii == dia) {
+            item.style.backgroundColor = 'rgba(60, 208, 208, 0.732)'
+        }
+        item.setAttribute('class',"calendar-day")
+        calendar.appendChild(item)
+        
+    }
+
     monthtxt.innerHTML = ``
     monthtxt.innerHTML = `Mês ${meses [mespg - 1]}`
 }
 avancar(data)
+
