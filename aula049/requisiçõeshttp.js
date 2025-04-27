0af307b8b0712cc5af7d528893452f8b1ed59db5
Luiz Gustavo Dos Santos Pereira <luizgustavosantospreira@gmail.com>
2025-04-27 19:26:12 -0300

    Update requisiçõeshttp.js

--- aula049/requisiçõeshttp.js
+++ aula049/requisiçõeshttp.js
@@ -1,15 +1,13 @@
 async function procurarcep() {
-    const cepselecionado = document.getElementById("cep").value
-    if (typeof cepselecionado != "string"
-    ) {
-        alert("Seu cep deve conter apenas números")
+    var ceps = document.getElementById('cep').value
+    var verif = parseInt(ceps)
+    console.log(verif)
+    if (!isNaN(verif)) {
+        const promise = await fetch(`https://brasilapi.com.br/api/cep/v1/${ceps}`).then(resposta => resposta.json())
+        document.getElementById(`resposta`).innerHTML = `Estado: ${promise.state}, cidade: ${promise.city} <br>
+        Bairro: ${promise.neighborhood}, Rua: ${promise.street}`
     }
     else {
-        let resposta = await fetch(`https://brasilapi.com.br/api/cep/v1/${cepselecionado}`).catch((resposta) => {
-            console.log(`erro: ${resposta}`)
-        }).then(resposta => resposta.json())
-        console.log(resposta)
-        document.getElementById(`resposta`).innerText = `seu Cep é: `
-        document.getElementById("resposta").innerHTML += String(`cidade:${resposta.city},bairro: ${resposta.neighborhood} ,<Br>Rua: ${resposta.street} ,estado: ${resposta.state} `)
+        alert("Insira o cep de maneira correta!")
     }
 }
