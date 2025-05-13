e2a64e177830e236f7eaf1288244c4eb23283d8d
Luiz Gustavo Dos Santos Pereira <luizgustavosantospreira@gmail.com>
2025-05-12 21:15:52 -0300

    Update TODO list

--- Weater/script.js
+++ Weater/script.js
@@ -1,14 +1,13 @@
 
 async function call() {
     var query = document.getElementById("query").value
+    if (query == "" || )
     const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=26f6a0a03bbf4d499f700617251105&q=${query}&aqi=no`).then(resposta => resposta.json())
     const display = document.getElementById("display")
     const humidade = document.getElementById("Humidade")
     const temperatura = document.getElementById("temperatura")
     const sensaçãotermica = document.getElementById("sensaçãotermica")
-    const humidadeimg = document.getElementById("imghumidade")
-    const temperaturaimg = document.getElementById("imgtemperatura")
-    display.style.display = "block"
+    display.style.animationPlayState = "running"
     console.log(promise)
     humidade.innerHTML = promise.current.humidity
     temperatura.innerHTML = `${promise.current.temp_c} C`
